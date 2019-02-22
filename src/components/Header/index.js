837e4bb57a7ce03e08a0fd2341a49113d7feaf67
Fullstack <fullstack@Fullstacks-Mac-mini.local>
2019-02-22 15:45:26 +0200

    Some fixes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -29,7 +29,7 @@ const Header = ({
       />
     </header>
 
-    {/* {isShow && <Authorization handleCloseModal={handleCloseModal} />} */}
+    {isShow && <Authorization handleCloseModal={handleCloseModal} />}
   </React.Fragment>
 )
 
