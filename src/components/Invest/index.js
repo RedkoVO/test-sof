dda27da8e9d0bfa96ca4f7ec6d4baf55d575b946
Fullstack <fullstack@Fullstacks-Mac-mini.local>
2019-03-18 12:24:38 +0200

    Structure and layout of invest page.

--- src/components/Invest/index.js
+++ src/components/Invest/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import cn from 'classnames'
 import withStyles from '@material-ui/core/styles/withStyles'
 
 import styles from './styles'
 
 const Invest = ({ classes }) => (
-  <div className={classes.root}>Invest</div>
+  <div className={classes.root}>
+    <div className={classes.investTitle}>Мои инвестиции</div>
+    <div className={classes.tabs}>
+      <div className={cn(classes.tab, 'active')}>Инвестиционный кошелек №1</div>
+      <div className={classes.tab}>Инвестиционный кошелек №2</div>
+      <div className={classes.tab}>+</div>
+    </div>
+    <div className={classes.wrWallet}>
+      <div className={classes.wallet}>Ваш ETH кошелек: 0000000000000000</div>
+      <div className={classes.invested}>Инвестировано: 0 ETH</div>
+      <div className={classes.earned}>Заработано: 0 ETH</div>
+      <div className={classes.wrButton}>
+        <button className={classes.button}>Вывести</button>
+        <Link to="/" className={classes.withdrawRight}>
+          Правила вывода
+        </Link>
+      </div>
+      <div className={classes.referralTitle}>Реферальная программа</div>
+      <p>Ваша реферальная ссылка</p>
+      <p>https://tilda.cc/page/?pageid=5175144</p>
+      <div className={classes.referrals}>
+        <p>Количество привлеченных рефералов: 0</p>
+        <p>Ваша реферальная ссылка Заработано с рефералов: 0 ETH</p>
+      </div>
+    </div>
+  </div>
 )
 
 Invest.propTypes = {
--- src/components/Invest/styles.js
+++ src/components/Invest/styles.js
@@ -1,3 +1,67 @@
 export default () => ({
-  root: {}
+  root: {
+    padding: '0 80px'
+  },
+  investTitle: {
+    color: '#000',
+    fontSize: '38px',
+    fontWeight: 600,
+    textAlign: 'center',
+    paddingBottom: '30px'
+  },
+  tabs: {
+    display: 'flex',
+    padding: '50px 0'
+  },
+  tab: {
+    marginRight: '15px',
+    padding: '12px 25px',
+    color: '#fff',
+    fontSize: '14px',
+    fontWeight: 600,
+    borderRadius: '30px',
+    backgroundColor: '#8a8a8a',
+    cursor: 'pointer',
+
+    '&.active': {
+      backgroundColor: '#000'
+    }
+  },
+  wrWallet: {
+    fontSize: '20px',
+    fontWeight: 400
+  },
+  wallet: {
+    marginBottom: '30px'
+  },
+  invested: {
+    marginBottom: '20px'
+  },
+  earned: {
+    marginBottom: '20px'
+  },
+  button: {
+    padding: '10px 40px',
+    color: '#ffffff',
+    fontSize: '14px',
+    fontWeight: 600,
+    borderRadius: '30px',
+    backgroundColor: '#000'
+  },
+  withdrawRight: {
+    marginLeft: '15px',
+    color: '#0060a1',
+    fontStyle: 'italic',
+    textDecoration: 'underline'
+  },
+  referralTitle: {
+    color: '#000',
+    fontSize: '38px',
+    fontWeight: 600,
+    textAlign: 'center',
+    padding: '70px 0'
+  },
+  referrals: {
+    paddingTop: '20px'
+  }
 })
